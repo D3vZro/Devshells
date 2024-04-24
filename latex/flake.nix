a53fd207b39436cf378ff6b31b267052d6d19295
Toni Nguyen <d3vzero@outlook.de>
2024-04-24 18:25:02 +0200

    latex: Init latex devshell

---
+++ latex/flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "A basic devshell";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs";
+    nixNeovim.url = "github:D3vZro/NixNeovim";
+  };
+
+  outputs = { self, nixpkgs, nixNeovim,... }:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+    nvim = nixNeovim.outputs.packages.${system}.nvim-latex;
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      # Interactive packages
+      packages = with pkgs; [
+        ripgrep
+        fd
+        nvim
+        pandoc
+        texlab
+      ];
+
+      # Build dependencies
+      inputsFrom = with pkgs; [
+        tesxliveFull
+      ];
+    };
+  };
+}
