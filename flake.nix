b8d04877672af9d9b0440f3d43e77b28b98e1d22
Toni Nguyen <d3vzero@outlook.de>
2024-04-24 18:29:12 +0200

    flake: Add latex template

--- flake.nix
+++ flake.nix
@@ -7,6 +7,11 @@
       description = "A basic devshell";
     };
 
+    templates.latex = {
+      path = ./latex;
+      description = "A devshell for editing and compiling LaTeX documents";
+    };
+
     defaultTemplate = self.templates.basic;
   };
 }
