3163807559977e2e029a91656ed8a18f48bd5fbc
Toni Nguyen <d3vzero@outlook.de>
2024-11-10 00:22:35 +0100

    svelte: Add new config

---
+++ svelte/flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "A devshell for svelte and tailwindcss";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs";
+    nixNeovim.url = "github:D3vZro/NixNeovim";
+  };
+
+  outputs = { self, nixpkgs, nixNeovim,... }:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+    nvim = nixNeovim.outputs.packages.${system}.svelte;
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      # Interactive packages
+      packages = with pkgs; [
+        nvim
+        svelte-language-server
+        tailwindcss-language-server
+        nodejs
+      ];
+    };
+  };
+}
